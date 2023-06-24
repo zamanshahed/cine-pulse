2a8631e856fa5efe64d65b47c5754eeb5e807110
zamanshahed <shaheduz.zaman@northsouth.edu>
2023-06-24 23:57:06 +0600

    initial animation loop worked with tailwind

--- app/components/framer-components/AnimatedOne.jsx
+++ app/components/framer-components/AnimatedOne.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import { motion } from "framer-motion"
 
 const AnimatedOne = () => {
-  return (
-    <div>AnimatedOne</div>
-  )
+    return (
+        <div>
+            <motion.div
+                className='w-20 h-20 bg-rose-500 text-center text-4xl font-extralight text-white italic'
+                animate={{
+                    scale: [2, 1, 2, 1, 2],
+                    rotate: [0, 0, 180, 180, 0],
+                    borderRadius: ["0%", "0%", "50%", "50%", "0%"]
+                }}
+                transition={{
+                    duration: 2,
+                    ease: "easeInOut",
+                    times: [0, 0.2, 0.5, 0.8, 1],
+                    repeat: Infinity,
+                    repeatDelay: 1
+                }}
+            />
+
+        </div>
+    )
 }
 
 export default AnimatedOne
