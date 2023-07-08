1ae2fe56c33a94507575edbc0489f2cbe412ba90
zamanshahed <shaheduz.zaman@northsouth.edu>
2023-07-08 08:43:44 +0600

    smooth accordion transition added

--- app/components/accordion/CustomAccordion.jsx
+++ app/components/accordion/CustomAccordion.jsx
@@ -27,10 +27,27 @@ const CustomAccordion = ({
                     animate={{
                         height: "auto",
                         opacity: 1,
+                        transition: {
+                            height: {
+                                duration: 0.4,
+                            },
+                            opacity: {
+                                duration: 0.25,
+                                delay: 0.15,
+                            },
+                        },
                     }}
                     exit={{
                         height: 0,
                         opacity: 0,
+                        transition: {
+                            height: {
+                                duration: 0.4,
+                            },
+                            opacity: {
+                                duration: 0.25,
+                            },
+                        },
                     }}
                 >
 
